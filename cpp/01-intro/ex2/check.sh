113e645c862990a0a1c30076d590f8c7574ad6d4
robertnen <robert.nenciu@yahoo.com>
2024-03-28 01:58:28 +0200

    Update check.sh

--- cpp/01-intro/ex2/check.sh
+++ cpp/01-intro/ex2/check.sh
@@ -2,12 +2,10 @@
 
 # recompilam in cazul in care s-a uitat
 make clean > /dev/null
-sleep 1
 make > /dev/null
-sleep 1
 
 # executam programul
-output=$(./basic_operation.bin  2>/dev/null)
+output=$(./basic_operations.bin  2>/dev/null)
 
 # verificam outputul
 if [ "$output" = "a + b = 8\na - b = 2\na * b = 15\na / b = 1\na % b = 2" ]; then
