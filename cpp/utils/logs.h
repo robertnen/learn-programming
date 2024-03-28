32f3b9073e9e0cbb3dd23a9957b1583b549264f5
robertnen <robert.nenciu@yahoo.com>
2024-03-28 11:54:50 +0200

    Update logs.h

--- cpp/utils/logs.h
+++ cpp/utils/logs.h
@@ -4,10 +4,29 @@
     #include <iostream>
 
     const std::string EXPRESIE = "Inlocuieste aici";
+    const int INLOCUIESTE = -1;
 
     template <typename T>
     void afiseaza(std::string msg, T result) {
         std::cout << msg << " = " << result << '\n';
+    }
+
+    void checkMax(int x, int y, int z, int maxim) {
+        int m = x;
+
+        if(m < y) m = y;
+        if(m < z) m = z;
+
+        std::cout << (maxim == m ? "Ai gasit maximul.\n" : "Raspuns gresit.\n");
+    }
+
+    void checkMin(int x, int y, int z, int minim) {
+        int m = x;
+
+        if(m > y) m = y;
+        if(m > z) m = z;
+
+        std::cout << (minim == m ? "Ai gasit minimul.\n" : "Raspuns gresit.\n");
     }
 
 #endif
