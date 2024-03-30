3043dc267ef3034fcc8dc3990183a16460ec58bc
robertnen <robert.nenciu@yahoo.com>
2024-03-31 01:51:50 +0200

    Update logs.h

--- cpp/utils/logs.h
+++ cpp/utils/logs.h
@@ -5,10 +5,20 @@
 
     const std::string EXPRESIE = "Inlocuieste aici";
     const int INLOCUIESTE = -1;
+    const std::string NUMAR = "(Pune un numar aici)";
+    const std::string OPERATIE = "(Pune o operatie aici)";
+    const std::string VARIABILA = "(Pune o variabila aici)";
+
+    int variabila;
 
     template <typename T>
     void afiseaza(std::string msg, T result) {
         std::cout << msg << " = " << result << '\n';
+    }
+
+    template <typename T>
+    bool test(T x, T y) {
+        return !(x - y);
     }
 
     void checkMax(int x, int y, int z, int maxim) {
