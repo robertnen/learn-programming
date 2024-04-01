d2abc406b4efea627c0dd6d104abd65240b21fed
robertnen <robert.nenciu@yahoo.com>
2024-04-02 02:52:48 +0300

    03-if-else Playground

---
+++ cpp/03-if-else/playground/playground.cpp
@@ -0,0 +1,18 @@
+#include "../../utils/logs.h"       //* nu sterge
+
+using namespace std;                //* nu sterge
+
+int main() {                        //* nu sterge
+
+    //? Aici testam diverse lucruri
+    //TODO: tema in caz ca nu ajungem pana la final aici sa o facem:
+    //* 1. Sa se verifice daca un caracter e litera (char c / ch)
+    //* 2. Sa se verifice daca un numar x e divizibil cu y
+    //* 3. Sa se verifice intr-un if daca 3 numere sunt in ordinea asta 4, 12
+    //*      sau 7, in caz contrar afisam "NU\n";
+    //* 4. Se da un interval [a, b] si 2 numere x si y. Sunt x si y in interval?
+    //* 5. Sa se verifice daca ultima cifra a doua numere este para sau daca
+    //*     suma ultimei cifre din fiecare e impara
+
+    return 0;                       //* nu sterge
+}                                   //* nu sterge
